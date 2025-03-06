252ce8953aff66599084cde32dea3c7184e0cc9d
Guillermo Cifre <guillecg13@gmail.com>
2025-03-06 15:57:04 +0100

    Modified

--- challenge_2/challange_2.ts
+++ challenge_2/challange_2.ts
@@ -1,17 +1,13 @@
 function createFrame(names: string[]): string {
-  let frame = "", decoratedNames = "", maxLengthName = ""
+  let frame = "", decoratedNames = ""
+  const longestName = names.toSorted((a, b) => b.length - a.length)
+  frame = `**${longestName[0].replace(longestName[0], "*").repeat(longestName[0].length)}**\n`
   names.forEach(name => {
-    if (name.length > maxLengthName.length) {
-      maxLengthName = name
-      frame = `**${name.replace(name, "*").repeat(name.length)}**\n`
-    }
-  })
-  names.forEach(name => {
-    decoratedNames += `* ${name}${maxLengthName.replace(maxLengthName, " ").repeat(maxLengthName.length - name.length)} *\n`
+    decoratedNames += `* ${name}${longestName[0].replace(longestName[0], " ").repeat(longestName[0].length - name.length)} *\n`
   })
   return frame + decoratedNames + frame.replace("\n", "")
 }
 
-createFrame(['midu'])
+createFrame(['midu', 'madeval', 'educalvolpz', 'midu'])
 
 export default createFrame;
